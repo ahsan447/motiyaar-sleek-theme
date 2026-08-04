9785c435b7f0e78bbee8d0e1c52a6b88aaa006d1
AhsanQureshi48 <qureshisunny55@gmail.com>
2026-08-04 18:37:47 +0500

    added slider

---
+++ assets/collection-pills-slider.js
@@ -0,0 +1,49 @@
+if (!customElements.get('collection-pills-slider')) {
+  customElements.define(
+    'collection-pills-slider',
+    class CollectionPillsSlider extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        this.selectors = {
+          sliderWrapper: '.collection-pills__items',
+        };
+
+        this.sliderWrapper = this.querySelector(this.selectors.sliderWrapper);
+        this.itemsDesktop = parseFloat(this.dataset.itemsDesktop) || 12;
+        this.itemsMobile = parseFloat(this.dataset.itemsMobile) || 2.2;
+        this.sliderInstance = false;
+
+        this.initSlider();
+      }
+
+      initSlider() {
+        if (typeof this.sliderInstance === 'object') return;
+
+        const columnGap = window.getComputedStyle(this.sliderWrapper).getPropertyValue('--column-gap');
+        const spaceBetween = parseFloat(columnGap.replace('rem', '')) * 10 || 8;
+
+        const sliderOptions = {
+          slidesPerView: this.itemsMobile,
+          spaceBetween: spaceBetween,
+          breakpoints: {
+            768: {
+              slidesPerView: this.itemsDesktop,
+              spaceBetween: spaceBetween,
+            },
+          },
+          pagination: false,
+          loop: false,
+          threshold: 2,
+          grabCursor: true,
+          allowTouchMove: true,
+        };
+
+        this.sliderInstance = new window.FoxTheme.Carousel(this.sliderWrapper, sliderOptions);
+        this.sliderInstance.init();
+      }
+    }
+  );
+}
