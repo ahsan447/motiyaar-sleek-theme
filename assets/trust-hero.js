0af2484fd2beb5e700d43c6de8d64d9be9c5696c
AhsanQureshi48 <qureshisunny55@gmail.com>
2026-08-05 22:56:18 +0500

    addd

--- assets/trust-hero.js
+++ assets/trust-hero.js
@@ -35,3 +35,37 @@ if (!customElements.get('trust-hero-quotes-slider')) {
   }
   customElements.define('trust-hero-quotes-slider', TrustHeroQuotesSlider);
 }
+
+if (!customElements.get('trust-hero-media-slider')) {
+  class TrustHeroMediaSlider extends HTMLElement {
+    connectedCallback() {
+      this.sliderWrapper = this.querySelector('.trust-hero__media-slider');
+      this.sliderInstance = false;
+
+      this.initSlider();
+    }
+
+    initSlider() {
+      if (!this.sliderWrapper) return;
+
+      const slidesCount = this.sliderWrapper.querySelectorAll('.swiper-slide').length;
+
+      const sliderOptions = {
+        modules: [FoxTheme.Swiper.Autoplay],
+        slidesPerView: 1,
+        loop: slidesCount > 1,
+        threshold: 2,
+        grabCursor: true,
+        allowTouchMove: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+      };
+
+      this.sliderInstance = new window.FoxTheme.Carousel(this.sliderWrapper, sliderOptions);
+      this.sliderInstance.init();
+    }
+  }
+  customElements.define('trust-hero-media-slider', TrustHeroMediaSlider);
+}
