a103816f3fd9070de935ceb50bf9d795ea319b0a
AhsanQureshi48 <qureshisunny55@gmail.com>
2026-08-20 20:09:19 +0500

    done

---
+++ assets/pair-perfectly-with-slider.js
@@ -0,0 +1,55 @@
+if (!customElements.get('pair-perfectly-with-slider')) {
+  customElements.define(
+    'pair-perfectly-with-slider',
+    class PairPerfectlyWithSlider extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        const slider = this.querySelector('.swiper');
+        if (!slider) return;
+
+        this.sliderOptions = {
+          slidesPerView: 2,
+          spaceBetween: 12,
+          loop: true,
+          grabCursor: true,
+          allowTouchMove: true,
+          threshold: 2,
+          navigation: {
+            prevEl: this.querySelector('.swiper-button-prev'),
+            nextEl: this.querySelector('.swiper-button-next'),
+          },
+          breakpoints: {
+            1024: {
+              slidesPerView: 4,
+            },
+          },
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          },
+        };
+
+        this.sliderInstance = new window.FoxTheme.Carousel(slider, this.sliderOptions, FoxTheme.Swiper.Autoplay);
+        this.sliderInstance.init();
+
+        this.fixQuickviewDuplicate();
+      }
+
+      fixQuickviewDuplicate() {
+        let modalIds = [];
+        Array.from(this.querySelectorAll('quick-view-modal')).forEach((modal) => {
+          const modalID = modal.getAttribute('id');
+          if (modalIds.includes(modalID)) {
+            modal.remove();
+          } else {
+            modalIds.push(modalID);
+          }
+        });
+      }
+    }
+  );
+}
